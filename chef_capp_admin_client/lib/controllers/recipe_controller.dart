0ec5d50848fd11748d5e61cd19f50448941ddad6
FizzyPopp <ardakizilcay@hotmail.com>
2020-09-01 22:27:51 -0400

    Ingredients providers and consumers setup (except dropdowns)

--- chef_capp_admin_client/lib/controllers/recipe_controller.dart
+++ chef_capp_admin_client/lib/controllers/recipe_controller.dart
@@ -26,3 +26,24 @@ class RecipeController extends ChangeNotifier {
 
   }
 }
+
+class IngredientController extends ChangeNotifier {
+  void onEdit(BuildContext context) {
+
+  }
+  void onDelete(BuildContext context) {
+
+  }
+  void onSave(BuildContext context) {
+
+  }
+  void ingredientNameChanged(String newText) {
+
+  }
+  void ingredientTagChanged(String newText) {
+
+  }
+  void ingredientPluralChanged(String newText) {
+
+  }
+}
