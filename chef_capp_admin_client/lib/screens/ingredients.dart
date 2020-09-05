fea8b66d3e8440a57c3759f2fdd6d88918d24db4
FizzyPopp <ardakizilcay@hotmail.com>
2020-09-04 23:13:53 -0400

    Ingredients providers and consumers setup (except dropdowns)

--- chef_capp_admin_client/lib/screens/ingredients.dart
+++ chef_capp_admin_client/lib/screens/ingredients.dart
@@ -118,6 +118,33 @@ class IngredientsHome extends StatelessWidget {
 class IngredientAdd extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const mockResults = <AppProfile>[
+      AppProfile('John Doe', 'jdoe@flutter.io',
+          'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4057996.jpg'),
+      AppProfile('Paul', 'paul@google.com',
+          'https://mbtskoudsalg.com/images/person-stock-image-png.png'),
+      AppProfile('Fred', 'fred@google.com',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Brian', 'brian@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('John', 'john@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Thomas', 'thomas@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Nelly', 'nelly@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Marie', 'marie@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Charlie', 'charlie@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Diana', 'diana@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Ernie', 'ernie@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('Gina', 'fred@flutter.io',
+          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+    ];
+
     return ChangeNotifierProvider<IngredientController>(
       create: (_) => IngredientController(),
       child: Padding(
@@ -296,6 +323,52 @@ class IngredientAdd extends StatelessWidget {
                       },
                     ),
                   ),
+                  ChipsInput(
+                    allowChipEditing: false,
+                    findSuggestions: (String query) {
+                      if (query.length != 0) {
+                        var lowercaseQuery = query.toLowerCase();
+                        return mockResults.where((profile) {
+                          return profile.name.toLowerCase().contains(query.toLowerCase()) || profile.email.toLowerCase().contains(query.toLowerCase());
+                        }).toList(growable: false)
+                          ..sort((a, b) => a.name
+                              .toLowerCase()
+                              .indexOf(lowercaseQuery)
+                              .compareTo(b.name.toLowerCase().indexOf(lowercaseQuery)));
+                      } else {
+                        return const <AppProfile>[];
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Select People",
+                    ),
+                    maxChips: 3,
+                    onChanged: (data) {
+                      print(data);
+                    },
+                    chipBuilder: (context, state, profile) {
+                      return InputChip(
+                        key: ObjectKey(profile),
+                        label: Text(profile.name),
+                        avatar: CircleAvatar(
+                          backgroundImage: NetworkImage(profile.imageUrl),
+                        ),
+                        onDeleted: () => state.deleteChip(profile),
+                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      );
+                    },
+                    suggestionBuilder: (context, state, profile) {
+                      return ListTile(
+                        key: ObjectKey(profile),
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(profile.imageUrl),
+                        ),
+                        title: Text(profile.name),
+                        subtitle: Text(profile.email),
+                        onTap: () => state.selectSuggestion(profile),
+                      );
+                    },
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -340,5 +413,28 @@ class IngredientAdd extends StatelessWidget {
       ));
     }
     return out;
+  }
+}
+
+class AppProfile {
+  final String name;
+  final String email;
+  final String imageUrl;
+
+  const AppProfile(this.name, this.email, this.imageUrl);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is AppProfile &&
+              runtimeType == other.runtimeType &&
+              name == other.name;
+
+  @override
+  int get hashCode => name.hashCode;
+
+  @override
+  String toString() {
+    return name;
   }
 }
